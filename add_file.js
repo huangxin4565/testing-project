298f9cbb3a0d479cfed0173c504647a039a7b047
huangxin4565 <huang.xin@aderaglobal.com>
2022-09-29 15:37:31 +0800

    add code

--- add_file.js
+++ add_file.js
@@ -4,3 +4,11 @@
     await this.getFunderFeatures();
     this.loaderService.display(false);
   }
+
+
+  async init() {
+    this.loaderService.display(true);
+    this.initRowParams();
+    await this.getFunderFeatures();
+    this.loaderService.display(false);
+  }
