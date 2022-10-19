8b4d8f711ba5f532adf00763f64ba90524c66b8a
hx <huang.xin@aderaglobal.com>
2022-10-19 17:51:46 +0800

    update test file

---
+++ test/service/invoice-setting-service.spec.js
@@ -0,0 +1,101 @@
+'use strict';
+
+//unit test libs
+const sinon = require('sinon');
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
+const faker = require('faker');
+const proxyquireStrict = require('proxyquire').noCallThru();
+
+//import mock objects
+const S3Mock = require('../commons/S3Mock');
+const EventMock = require('../commons/EventMock');
+const ContextMock = require('../commons/ContextMock');
+
+//import db sequelize
+const { db, sequelize } = require('../../models/index');
+
+let userUtilsModule = {
+	getUserInfo: (_) => {},
+};
+let mockDocID = undefined;
+
+describe('unit testing for invoice setting service', async function () {
+	before(async function () {
+		// Create a fake document record to test
+		// mockDocID = await db.documents.create({
+		//     document_category_id: 1,
+		//     document_uuid: "123",
+		//     reference_type: "BUSINESS_ENTITY",
+		//     document_path: "SG/230",
+		//     document_uuid: "fakepath"
+		// });
+		// console.log("mockDocID: " + mockDocID.document_id);
+	});
+	after(async function () {
+		//clean up if test fails
+		// try {
+		//     await db.documents.destroy({
+		//         where: {
+		//             document_path: "SG/230",
+		//             document_uuid: "fakepath"
+		//         }
+		//     })
+		// } catch (err) { }
+	});
+
+	it('check if get document category works properly', async function () {
+		this.timeout(5000);
+		process.env.bucket_name = 'test_bucket';
+		process.env.env = 'local';
+
+		//generate fake event and context
+		let event = EventMock;
+		event.context.username = 'xiaoyong.fang@nufindata.com456';
+		event.data = {
+			// category_name: "CUSTOMER INVOICES",
+			parent_name: 'CUSTOMER INVOICES',
+		};
+		let context = ContextMock;
+
+		//test functions
+		// sinon.stub(userUtilsModule, "getUserInfo").returns({
+		//     user: {
+		//         business_entity_id: 1,
+		//         business_entity: {
+		//             country_code: 1
+		//         }
+		//     }
+		// });
+
+		// mock required modulesn
+		var service = proxyquireStrict('../../models/service/address-service', {
+			//    '../../../modules/onboarding/cognito/'
+			// "../../utils/user-utils": userUtilsModule,
+			'aws-sdk': {
+				S3: S3Mock,
+			},
+		});
+
+		// get business result from function
+		let resp = await service.create_item_with_address(
+			{ street_name: 'kg', bank_account_number: 'testbank' },
+			'bank_account',
+			11,
+			db,
+		);
+		let result = JSON.stringify(resp);
+		console.log('result: ' + result);
+
+		//retrieve the result
+		// let mockDocObj = await db.documents.findOne({
+		//     where: {
+		//         document_id: mockDocID.document_id
+		//     }
+		// });
+
+		// expect(mockDocObj).to.be.null;
+	});
+});
