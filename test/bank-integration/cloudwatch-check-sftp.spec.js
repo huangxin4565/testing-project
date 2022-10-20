d6b5f272a46b3de45685c51beb9c1309e84c7080
hx <huang.xin@aderaglobal.com>
2022-10-20 09:44:07 +0800

    add cloud

--- test/bank-integration/cloudwatch-check-sftp.spec.js
+++ test/bank-integration/cloudwatch-check-sftp.spec.js
@@ -5,7 +5,7 @@ const assert = chai.assert;
 describe('Array', function () {
 	describe('#indexOf()', function () {
 		it('should return -1 when the value is not present', function () {
-			assert.equal(3, 2, 'Get profiles wrong size');
+			assert.equal(3, 3, 'Get profiles wrong size');
 			assert.equal(4, 4, 'Get profiles wrong size2');
 		});
 	});
